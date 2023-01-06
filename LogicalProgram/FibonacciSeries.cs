1b16cce5bfaef89120cd6031dea85b353f31315d
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-06 23:55:36 +0530

    Welcome To Logical Problem

---
+++ LogicalProgram/FibonacciSeries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class FibonacciSeries
+    {
+        public static void Fibonacci()
+        {
+            int x=0, y=1,z= 0;
+            Console.WriteLine("Enter value");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("-----\n0 \n1");
+            for (int i = 2; i < input; i++)
+            {
+                z = x+y;
+                x = y;
+                y = z;
+                Console.WriteLine(z);
+               
+            }
+        }
+    }
+}
---
+++ LogicalProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Programs");
+            Console.WriteLine("Select the Program\n1.Fibonacci Series");
+            Console.WriteLine("Choose option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibonacciSeries.Fibonacci();
+                    break;
+            }
+        }
+    }
+}
