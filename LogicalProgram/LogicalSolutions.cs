3a2b3561aac328f86223464aa03ebd4f0e5b905b
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-08 16:10:35 +0530

    [SUNIL]:Added Reverse Number Code

--- LogicalProgram/LogicalSolutions.cs
+++ LogicalProgram/LogicalSolutions.cs
@@ -10,17 +10,76 @@ namespace LogicalProgram
     {
         public static void Fibonacci()
         {
-            int x = 0, y = 1, z = 0;
+            int x=0, y=1,z= 0;
             Console.WriteLine("Enter value");
             int input = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("-----\n0 \n1");
             for (int i = 2; i < input; i++)
             {
-                z = x + y;
+                z = x+y;
                 x = y;
                 y = z;
                 Console.WriteLine(z);
+               
             }
+        }
+        //CheckPerfectNumber
+        public static void FindPerfectNumber()
+        {
+            int result = 0;
+            Console.WriteLine("Enter number to check for perfect number");
+            int input = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < input; i++)
+            {
+                if (input % i == 0)
+                {
+                    result = result + i;
+                }
+            }
+            if (result == input)
+            {
+                Console.WriteLine(input + " is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(input + " is not a perfect number");
+            }
+        }
+        //checkPrimeNumbers
+        public static void PrimeNumber()
+        {
+            int count = 0;
+            Console.WriteLine("Enter the Number : ");
+            int number = Convert.ToInt32(Console.ReadLine());           
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 0)
+                Console.WriteLine(number + " is prime Number");
+            else
+                Console.WriteLine(number + " is not a Prime Number!");
+        }
+        // CheckingReverseNumber
+        public static void Checkingreversenumber()
+        {
+            int lastNumber, reverseNumber = 0;
+            Console.WriteLine("Enter number to reverse");
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input = " + input);
+            while (input != 0)
+            {
+                if (input != 0)
+                {
+                    lastNumber = (input % 10);
+                    input = (input / 10);
+                    reverseNumber = (reverseNumber * 10) + lastNumber;
+                }
+            }
+            Console.WriteLine("Reversed input = " + reverseNumber);
         }
     }
 }
--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -5,13 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
-            Console.WriteLine("Select the Program\n1.Fibonacci Series");
+            Console.WriteLine("Select the Program\n1.Fibonacci Series\n2.PerfectNumber\n3.PrimeNumber\n4.ReverseNumber");
             Console.WriteLine("Choose option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     LogicalSolutions.Fibonacci();
+                    break;
+                case 2:
+                    LogicalSolutions.FindPerfectNumber();
+                    break;
+                case 3:
+                    LogicalSolutions.PrimeNumber();
+                    break;
+                case 4:
+                    LogicalSolutions.Checkingreversenumber();
                     break;
             }
         }
