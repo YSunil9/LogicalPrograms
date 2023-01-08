2eba190edab341d5e6a28415842ea3d54f976552
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-08 16:03:00 +0530

    [SUNIL]:Added fibonacci Number Code

--- LogicalProgram/Program.cs
+++ LogicalProgram/Program.cs
@@ -5,19 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Logical Programs");
-            Console.WriteLine("Select the Program\n1.Fibonacci Series\n2.PerfectNumber\n3.PrimeNumber");
+            Console.WriteLine("Select the Program\n1.Fibonacci Series");
             Console.WriteLine("Choose option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     LogicalSolutions.Fibonacci();
-                    break;
-                case 2:
-                    LogicalSolutions.FindPerfectNumber();
-                    break;
-                case 3:
-                    LogicalSolutions.PrimeNumber();
                     break;
             }
         }
